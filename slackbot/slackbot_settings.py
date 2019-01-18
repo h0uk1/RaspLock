e8870587ee849036a8fae1df3bf2d91afeb7a3b3
h0uk1 <40357304+h0uk1@users.noreply.github.com>
2019-01-18 16:46:44 +0900

    Update slackbot_settings.py

--- slackbot/slackbot_settings.py
+++ slackbot/slackbot_settings.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 # botアカウントのトークンを指定
-API_TOKEN = "xoxb-520771626433-522430213367-gvAA03YdEs8yUgZsh62s7Rvs"
+API_TOKEN = "xxxxxxxxxxxxxxxxxxxxx"
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "何言ってんだこいつ"
